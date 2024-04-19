59cca0fdeb9af97c5c44f78148325c1dcafe28b4
Eeva <evae0718@gmail.com>
2024-04-19 20:02:41 +0200

    added health script and .gitignore

--- bulletspin/Assets/Scripts/Health.cs
+++ bulletspin/Assets/Scripts/Health.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    public int lives;
+
+    private int health;
+
     void Start()
     {
-        
+        health = lives;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void updateHealth()
     {
-        
+        health -= 1;
+
+        Debug.Log("update");
+        Debug.Log(health);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("dead");
+            Debug.Log(health);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        updateHealth();
+        Debug.Log("crash");
     }
 }
